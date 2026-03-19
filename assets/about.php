780abdc375c2eb480b23a066e1fb138a538479f9
Kalana Madushan <62831247+kalanamadushan963@users.noreply.github.com>
2026-03-19 18:07:05 +0400

    Update about.php

--- assets/about.php
+++ assets/about.php
@@ -1,1 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    <title>Eyestudio Real Estate - Web Project</title>
+
+    <!-- =========== CSS Area =========== -->
+    <?php include 'include/css.inc.php'; ?>
+    <!-- =========== CSS Area =========== -->
+
+</head>
+
+
+<!-- page wrapper -->
+
+<body>
+
+    <div class="boxed_wrapper">
+
+        <!-- =========== Header Area =========== -->
+        <?php include 'include/header-mobile-menu.inc.php'; ?>
+        <!-- =========== Header Area =========== -->
+
+        <!--Page Title-->
+        <section class="page-title centred" style="background-image: url(assets/images/background/page-title-5.jpg);">
+            <div class="auto-container">
+                <div class="content-box clearfix">
+                    <h1>About Us</h1>
+                    <ul class="bread-crumb clearfix">
+                        <li><a href="index.html">Home</a></li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!--End Page Title-->
+
+
+        <!-- about-section -->
+        <section class="about-section about-page pb-0">
+            <div class="auto-container">
+                <div class="inner-container">
+                    <div class="row align-items-center clearfix">
+                        <div class="col-lg-6 col-md-12 col-sm-12 image-column">
+                            <div class="image_block_2">
+                                <div class="image-box">
+                                    <figure class="image"><img src="assets/images/resource/about-1.jpg" alt=""></figure>
+                                    <div class="text wow fadeInLeft animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                                        <h2>20</h2>
+                                        <h4>Years of <br />Experience</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-12 col-sm-12 content-column">
+                            <div class="content_block_3">
+                                <div class="content-box">
+                                    <div class="sec-title">
+                                        <h5>About</h5>
+                                        <h2>Hi, I’m Simrrun Rupani</h2>
+                                    </div>
+                                    <div class="text">
+                                        <p>Dolor sit amet consectetur elit sed do eiusmod tempor incididunt labore et dolore magna aliqua enim ad minim veniam quis nostrud exercitation ullamco laboris aliquip ex ea commodo consequat duis aute irure.</p>
+                                        <p>dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat.</p>
+                                    </div>
+                                    <ul class="list clearfix">
+                                        <li>consectetur elit sed do eius</li>
+                                        <li>consectetur elit sed</li>
+                                    </ul>
+                                    <div class="btn-box">
+                                        <a href="contact.html" class="theme-btn btn-one">Contact With Me</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- about-section end -->
+
+
+        <!-- feature-style-three -->
+        <section class="feature-style-three centred pb-110">
+            <div class="auto-container">
+                <div class="sec-title">
+                    <h5>Our Services</h5>
+                    <h2>Property Services</h2>
+                </div>
+                <div class="three-item-carousel owl-carousel owl-theme owl-nav-none dots-style-one">
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-1"></i></div>
+                            <h4>Excellent Reputation</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-26"></i></div>
+                            <h4>Best Local Agents</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-21"></i></div>
+                            <h4>Personalized Service</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-1"></i></div>
+                            <h4>Excellent Reputation</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-26"></i></div>
+                            <h4>Best Local Agents</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-21"></i></div>
+                            <h4>Personalized Service</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-1"></i></div>
+                            <h4>Excellent Reputation</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-26"></i></div>
+                            <h4>Best Local Agents</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                    <div class="feature-block-two">
+                        <div class="inner-box">
+                            <div class="icon-box"><i class="icon-21"></i></div>
+                            <h4>Personalized Service</h4>
+                            <p>Lorem ipsum dolor sit consectetur sed eiusm tempor incididunt dolore magna.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- feature-style-three end -->
+
+
+        <!-- cta-section -->
+
+        <!--
+        <section class="cta-section alternate-2 pb-240 centred" style="background-image: url(assets/images/background/cta-1.jpg);">
+            <div class="auto-container">
+                <div class="inner-box clearfix">
+                    <div class="text">
+                        <h2>Looking to Buy a New Property or <br />Sell an Existing One?</h2>
+                    </div>
+                    <div class="btn-box">
+                        <a href="property-details.html" class="theme-btn btn-three">Rent Properties</a>
+                        <a href="index.html" class="theme-btn btn-one">Buy Properties</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+-->
+        <!-- cta-section end -->
+
+
+        <!-- funfact-section -->
+        <section class="funfact-section centred">
+            <div class="auto-container">
+                <div class="inner-container wow slideInLeft animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                    <div class="row clearfix">
+                        <div class="col-lg-3 col-md-6 col-sm-12 funfact-block">
+                            <div class="funfact-block-one">
+                                <div class="inner-box">
+                                    <div class="count-outer count-box">
+                                        <span class="count-text" data-speed="1500" data-stop="1270">0</span>
+                                    </div>
+                                    <p>Total Professionals</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 funfact-block">
+                            <div class="funfact-block-one">
+                                <div class="inner-box">
+                                    <div class="count-outer count-box">
+                                        <span class="count-text" data-speed="1500" data-stop="2350">0</span>
+                                    </div>
+                                    <p>Total Property Sell</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 funfact-block">
+                            <div class="funfact-block-one">
+                                <div class="inner-box">
+                                    <div class="count-outer count-box">
+                                        <span class="count-text" data-speed="1500" data-stop="2540">0</span>
+                                    </div>
+                                    <p>Total Property Rent</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 funfact-block">
+                            <div class="funfact-block-one">
+                                <div class="inner-box">
+                                    <div class="count-outer count-box">
+                                        <span class="count-text" data-speed="1500" data-stop="8270">0</span>
+                                    </div>
+                                    <p>Total Customers</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- funfact-section end -->
+
+
+        <!-- main-footer -->
+        <!-- =========== Footer Area =========== -->
+        <?php include 'include/footer.inc.php'; ?>
+        <!-- =========== Footer Area =========== -->
+        <!-- main-footer end -->
+
+
+
+        <!--Scroll to top-->
+        <button class="scroll-top scroll-to-target" data-target="html">
+            <span class="fal fa-angle-up"></span>
+        </button>
+    </div>
+
+
+    <!-- jequery plugins -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/validation.js"></script>
+    <script src="assets/js/jquery.fancybox.js"></script>
+    <script src="assets/js/appear.js"></script>
+    <script src="assets/js/scrollbar.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/jQuery.style.switcher.min.js"></script>
+    <script src="assets/js/jquery-ui.js"></script>
+    <script src="assets/js/product-filter.js"></script>
+
+    <!-- main-js -->
+    <script src="assets/js/script.js"></script>
+
+</body><!-- End of .page_wrapper -->
+
+</html>
